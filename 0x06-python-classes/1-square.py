b8195faa1f9941859cd3a4cc564ad55e4028099b
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-06-23 00:18:05 +0100

    crete a private field in square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Module contains: class square"""
+
+
+class Square:
+    """
+        Square: defines a square
+        Atrribitues:
+            size
+        Method:
+            __init__
+    """
+    def __init__(self, size):
+        """
+            Initialization of attributes for all instances
+            Args:
+                size
+        """
+        self.__size = size
