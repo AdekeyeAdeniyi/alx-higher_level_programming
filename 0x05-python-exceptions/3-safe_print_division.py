dd930e7337aa997e63f1a2a9ea978c426390cfed
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-06-22 13:14:50 +0100

    update code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         temp = a / b
     except ZeroDivisionError:
-        temp - None
+        temp = None
     finally:
         print("Inside result: {}".format(temp))
     return temp
