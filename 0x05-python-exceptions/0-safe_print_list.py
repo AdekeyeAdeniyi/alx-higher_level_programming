560264bdbe48fda0f1e731d942708cb83efe1fff
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-06-22 12:19:21 +0100

    print items in list using try and except block

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            count += 1
+            print("{}".format(my_list[i]))
+    except IndexError:
+        pass
