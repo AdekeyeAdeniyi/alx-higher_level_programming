ec00b037b6832f2bb503f44e8ee837d338c3b0cd
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-08-10 20:53:52 +0100

    list record with condition

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the table
-SELECT `name`, `score` 
-FROM `second_table`
-WHERE `name` IS NOT NULL
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
 ORDER BY score DESC;
