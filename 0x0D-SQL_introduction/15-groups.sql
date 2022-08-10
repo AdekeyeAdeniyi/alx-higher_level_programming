748eaccaa6a02dfb42fda01bd899b1d414966bed
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-08-10 20:32:28 +0100

    total number of records with same value

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- Lists the number of records with the same score.
 
-SELECT `score`, COUNT(DISTINCT `score`) AS `number`
+SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
 ORDER BY `number` DESC;
