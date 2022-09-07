d17611a4dd4e9f984cf0557d2a46a3c7fa38748a
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-07 12:36:09 +0100

    update code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const dict = require('./100-data').dict;
-console.log(dict)
+console.log(dict);
 
-let newDict = {};
+const newDict = {};
 
-for (let [key, value] of Object.entries(dict)) {
-    if(!newDict[value]) {
-        newDict[value] = [];
-        newDict[value].push(key);
-    }else {
-        newDict[value].push(key);
-    }
-};
+for (const [key, value] of Object.entries(dict)) {
+  if (!newDict[value]) {
+    newDict[value] = [];
+    newDict[value].push(key);
+  } else {
+    newDict[value].push(key);
+  }
+}
 
-return newDict;
+console.log(newDict);
