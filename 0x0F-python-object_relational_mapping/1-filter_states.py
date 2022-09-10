8bb9d68ab230832677b6adf7038a383461f363fa
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-10 15:03:15 +0100

    print all data with capital N from database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,9 @@ if __name__ == '__main__':
                          port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE\
+                    name LIKE BINARY 'N%'\
+                    ORDER BY id ASC")
 
     data = cursor.fetchall()
 
