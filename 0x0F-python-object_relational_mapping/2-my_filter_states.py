cb47fed5324eab633810457281956b623702db24
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-11 19:37:14 +0100

    print all data that matche a given string argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    With capital `N`
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    sql =   "SELECT * FROM states \
+            WHERE name = '{}' \
+            ORDER BY states.id ASC".format(argv[4])
+
+    cur.execute(sql)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
