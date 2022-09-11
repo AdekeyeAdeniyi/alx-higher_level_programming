93fedc34ff4585db884c7976092cbf1a9f58aa2f
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-11 19:02:05 +0100

    print all state with capital N order by id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 """
-    Script that lists all states from the database hbtn_0e_0_usa
-    with string name starting with N (upper N) 
+    A script that lists all states from the database hbtn_0e_0_usa
+    Username, password and database names are given as user args
 """
+
 
 import sys
 import MySQLdb
@@ -11,22 +12,18 @@ import MySQLdb
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
-
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        host='localhost',
+                        port=3306)
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE BINARY 'N%'\
-                    ORDER BY id ASC")
+    sql = """
+        SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id DESC
+    """
+
+    cursor.execute(sql)
 
     data = cursor.fetchall()
 
-    for row in data:
-        print(row)
-
-    cursor.close()
-    db.close()
-    
+    print(data)
