8cb781a9432340a9216ad038d00f21e228aea0b2
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-12 14:15:59 +0100

    print all cities

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -16,6 +16,7 @@ class City(Base):
         name (str): city name
         state_id (int): link to state id
     """
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
