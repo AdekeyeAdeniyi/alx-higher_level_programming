c6847574c8685bef5db278a14ea762a84bc00c63
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-23 15:40:04 +0100

    update code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-## Prints the size of the body of the response
-
-curl -sI "${1}" | grep 'content-length' | awk '{print $2}'
+#Prints the size of the body of the response
+curl -sI "$1" | grep "content-length" | awk '{print $2}'
