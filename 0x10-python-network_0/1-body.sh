024e7107e6d85f664d2761cdf15d8d83a02433f4
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-09-23 21:48:23 +0100

    print rrsponse if status is ok

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, print response body if status code = 200
-if [ "$(curl -sI "$1"| head -n 1 | cut -d$' ' -f2 )" -eq '200' ]; then curl -s "$1"; fi
+if [ "$(curl -sLw "%{http_code}" "$1" -o /dev/null)" -eq "200" ]; then curl -sL "$1"; fi
