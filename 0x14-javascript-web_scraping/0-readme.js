3f70fca77fcbfdd0249771c793a82d1f4d2c6b3a
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-20 07:24:41 +0100

    print content in a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 /* Read and print content in a file*/
-const fs = require('fs/promises');
-const { argv } = require('process');
-const path = require('path');
 
 const fileName = argv[2];
+const file = process.argv[2];
+const fs = require('fs');
 
-async function readFile (filename) {
-  try {
-    const data = await fs.readFile(
-      path.join(__dirname, `${filename}`),
-      { encoding: 'utf8' }
-    );
+fs.readFile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
     console.log(data);
-  } catch (err) {
-    console.log(err);
   }
-}
-
-readFile(fileName);
+});
